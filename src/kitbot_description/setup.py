c4136af7573909333a48f04296a0b47eaabd015e
Woodward Spivey <woodyspivey@gmail.com>
2026-09-21 13:20:43 -0400

    fix urdf to use the correct package name, and have collider meshes; make setup.py include kitbot description files

--- src/kitbot_description/setup.py
+++ src/kitbot_description/setup.py
@@ -1,6 +1,20 @@
+import os
+
 from setuptools import find_packages, setup
 
 package_name = 'kitbot_description'
+
+
+def data_files_from(path):
+    data_files = []
+    for root, _dirs, files in os.walk(path):
+        if files:
+            data_files.append((
+                os.path.join('share', package_name, root),
+                [os.path.join(root, name) for name in files],
+            ))
+    return data_files
+
 
 setup(
     name=package_name,
@@ -10,7 +24,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-    ],
+    ] + data_files_from('urdf') + data_files_from('meshes'),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='poultry',
